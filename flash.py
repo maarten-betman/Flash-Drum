5bf6df519a8df7b535215ed3117fe0b723fcb24a
Alfonso Moran <alfonso.amt@outlook.com>
2021-07-06 17:59:10 -0500

    Bubble and Dew calculations for Temperature are available.

--- flash.py
+++ flash.py
@@ -1,33 +1,115 @@
-from process_units import *
+from gekko import GEKKO
 
 class FlashDrum():
     def __init__(self):
         # A flas drum has one inlet stream and two outlet stream. This is how they are built.
         # They a dictionary type, {'molar flow': F, 'composition': z, 'enthanply': h, 'temperature': T, 'pressure': P}
+        # This class only works with pressure in kPa and temperature in K.
         self.feed = {'molar flow': None, 'composition': None, 'enthanply': None, 'temperature': None, 'pressure': None}
         self.gas = {'molar flow': None, 'composition': None, 'enthanply': None, 'temperature': None, 'pressure': None}
         self.liquid = {'molar flow': None, 'composition': None, 'enthanply': None, 'temperature': None, 'pressure': None}
         self.mode = 'Isothermal'
         self.heat = 0.0
-        self.pressure = Pressure(1.0, 'atm')
+        self.pressure = 1.0
 
     def setFeedStream(self, inputF):
         self.feed = inputF
 
     def getFeedStream(self):
         for key,val in self.feed.items():
-            print(str(key) + ': ' + str(val.value) + ' ' + val.units)
+            if val != None:
+                print(str(key) + ': ' + str(val))
 
     def setGasStream(self, outputG):
         self.gas = outputG
 
     def getGasStream(self):
         for key,val in self.gas.items():
-            print(str(key) + ': ' + str(val.value) + ' ' + val.units)
+            if val != None:
+                print(str(key) + ': ' + str(val) )
 
     def setLiquidStream(self, outputL):
         self.liquid = outputL
 
     def getLiquidsStream(self):
         for key,val in self.liquid.items():
-            print(str(key) + ': ' + str(val.value) + ' ' + val.units)
+            if val != None:
+                print(str(key) + ': ' + str(val) )
+
+    def idealK(self, T, P, f, fp = {}):
+        Psat = f(T, **fp)
+        return Psat / P
+
+
+    def isothermal(self, T, P, f, fp = {}):
+        self.gas['temperature'] = T
+        self.gas['pressure'] = P
+        self.liquid['temperature'] = T
+
+
+        # K's 
+        Ki = {}
+        for key in self.feed['composition'].value.keys():
+
+            Ki[key] = self.idealK(T.value,P.value, f['Antoine'], fp[key])
+
+        m = GEKKO()
+        Psi = m.Var(value=0.5, lb= 0.0, ub = 1.0)
+        x = sum([((self.feed['composition'].value[key] * (1 - Ki[key])) / (1 + Psi * (Ki[key] - 1))) for key in Ki.keys()])
+        m.Equation([x == 0])
+        m.solve(disp=False)  
+        self.gas['molar flow'] = MolarFlow(Psi.value[0] * self.feed['molar flow'].value, self.feed['molar flow'].units)
+        self.feed['molar flow'] = MolarFlow(self.feed['molar flow'].value - self.gas['molar flow'].value, self.gas['molar flow'].units)
+
+        for key in self.Ki.keys():
+            self.liquid['composition'].value['key'] = (self.feed['composition'].value[key]) / (1 + Psi.value[0] * (Ki[key] - 1))
+            self.gas['composition'].value['key'] = self.liquid['composition'].value['key']  * Ki[key]
+
+    def bubbleT(self, P, f, fp = {}):
+
+        m = GEKKO()
+        t = 0
+        for key in self.feed['composition'].keys():
+
+            t += f['AntoineInv'](P,**fp['AntoineInv'][key])
+        
+        t = round(t / len(self.feed['composition']))
+        T = m.Var(value = t, lb = 0.0, ub = 2000.0)
+        # K's 
+        Ki = {}
+
+        for key in self.feed['composition'].keys():
+            Ki[key] = self.idealK(T,P, f['Antoine'], fp['Antoine'][key])
+        
+        x = sum([self.feed['composition'][key] * Ki[key] for key in Ki.keys()])
+        m.Equation([x - 1 == 0])
+        m.solve(disp=False) 
+
+        return round(T.value[0], 2)
+
+    def dewT(self, P, f, fp = {}):
+
+        m = GEKKO()
+        t = 0
+        for key in self.feed['composition'].keys():
+
+            t += f['AntoineInv'](P,**fp['AntoineInv'][key])
+        
+        t = round(t / len(self.feed['composition']))
+
+
+        T = m.Var(value = t, lb = 0.0, ub = 2000.0)
+        # K's 
+        Ki = {}
+
+        for key in self.feed['composition'].keys():
+            Ki[key] = self.idealK(T,P, f['Antoine'], fp['Antoine'][key])
+
+        x = sum([self.feed['composition'][key] / Ki[key] for key in Ki.keys()])
+        m.Equation([x - 1 == 0])
+        m.solve(disp=False) 
+
+        return round(T.value[0], 2)
+
+
+      
--- tests.py
+++ tests.py
@@ -1,3 +1,34 @@
 from flash import FlashDrum
+from gekko import GEKKO
+import numpy as np
 
-flash = FlashDrum()
+
+def Antoine(T, A, B, C):
+    P = pow(10 , (A - (B / ((T - 273.15) + C))))
+    return P * .133322
+
+
+def AntoineInv(P, A, B, C):
+    T = ((B / (A - np.log10(P/.133322))) - C) + 273.15
+    return round(T, 2)
+
+
+if __name__ == '__main__':
+    flash = FlashDrum()
+    C1 = 'Benzene'
+    C2 = 'Toluene'
+    z = {C1: 0.5, C2: 0.5}
+    pB = {'A':6.89272, 'B':1203.531, 'C':219.888}
+    pT = {'A':6.95805, 'B':1346.773, 'C':219.693}
+    f = {'Antoine': Antoine, 'AntoineInv' : AntoineInv}
+    fp = {'Antoine': {C1: pB, C2: pT}, 'AntoineInv' : {C1: pB, C2: pT}}
+
+    feedStream = {'molar flow': 100,
+                'composition': z}
+
+    flash.setFeedStream(feedStream)
+    flash.getFeedStream()
+
+    T = flash.dewT(101.325, f, fp)
+    print("{:.2f}".format(T-273.15))  
+ 
